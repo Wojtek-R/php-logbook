ab84b81fe05efd57b768fcdc0cc68d9be494a2dd
Wojtek-R <qdxgm4uz>
2021-02-24 18:44:05 +0000

    exercises 6,7,8 and 9 completed.

---
+++ Week2/wk2ex8.php
@@ -0,0 +1,13 @@
+<?php
+$topModules[0] = "Internet Systems Development";
+$topModules[1] = "Programming 1";
+$topModules[2] = "Programming 2";
+$topModules[3] = "OOAD";
+$topModules[4] = "Software Engineering";
+$topModules[5] = "Open Source Systems";
+$topModules[6] = "Web Services";
+
+for($count = 0;$count < 7;$count++){
+    echo ($count+1)." module is $topModules[$count] <br/>";
+}
+?>
