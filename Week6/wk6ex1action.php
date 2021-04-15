bab90a58ceb51472551ddd26fda9614d5e3e2f82
Wojtek-R <qdxgm4uz>
2021-04-15 21:46:52 +0100

    Week 6

--- Week6/wk6ex1action.php
+++ Week6/wk6ex1action.php
@@ -1,17 +1,38 @@
 <?php
 $sql = "INSERT INTO test (name,email,phoneNumber) ";
-$sql = $sql . " values ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]’)";
+$sql = $sql . " VALUES ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]')";
 
 // Connect to server and select database
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "db1_gwalke01";
+
+// Create connection
+$link = new mysqli($servername, $username, $password,$database);
+
+// Check connection
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+}
+echo "Connected successfully";
+
+// Execute sql statement
+if (mysqli_query($link, $sql)) {
+    echo "<br/>New record created successfully";
+} else {
+    echo "Error: " . $sql . "".mysqli_error($link);
+}
+
+$sql = "SELECT * FROM test";
 
 // Execute sql statement
 
-$sql = "SELECT * from test";
+//$result = $link->query($sql); Object Oriented style
+$result = mysqli_query($link,$sql);
 
-// Execute sql statement
-
-while ($row = mysqli_fetch_assoc($result))
+while ($row = $result->fetch_assoc())
 {
-    echo "$row[name]  $row[email]  $row[phone_number] <br/>";
+    echo "<br/>$row[name]  $row[email]  $row[phoneNumber] ";
 }
 ?>
